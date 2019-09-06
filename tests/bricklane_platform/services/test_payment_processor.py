357d48343436629814b48e3505e32505455ca32d
Moses Mugisha <mossplix@gmail.com>
2019-09-06 12:09:09 +0300

    more bank payments ctest coverage

--- tests/bricklane_platform/services/test_payment_processor.py
+++ tests/bricklane_platform/services/test_payment_processor.py
@@ -16,26 +16,39 @@ class TestPaymentProcessor(unittest.TestCase):
 
     def setUp(self):
         self.payment_processor = PaymentProcessor()
+        self.card_fixture = get_path("card_payments_mixed.csv")
+        self.bank_fixture = get_path("bank_payments.csv")
+        self.empty_card_fixture = get_path("card_payments_empty.csv")
+        self.empty_bank_fixture = get_path("bank_payments_empty.csv")
 
     def test_get_payments(self):
-        fixture = get_path("card_payments_mixed.csv")
 
-        payments = self.payment_processor.get_payments(fixture, "card")
-        self.assertEqual(len(payments), 3)
-        self.assertEqual(payments[0].card.card_id, 30)
-        self.assertEqual(payments[1].card.card_id, 45)
-        self.assertEqual(payments[2].card.card_id, 10)
+        card_payments = self.payment_processor.get_payments(
+            self.card_fixture, "card")
+        self.assertEqual(len(card_payments), 3)
+        self.assertEqual(card_payments[0].card.card_id, 30)
+        self.assertEqual(card_payments[1].card.card_id, 45)
+        self.assertEqual(card_payments[2].card.card_id, 10)
+
+        bank_payments = self.payment_processor.get_payments(
+            self.bank_fixture, "bank")
+        self.assertEqual(len(bank_payments), 2)
+        self.assertEqual(bank_payments[0].bank.bank_account_id, '20')
+        self.assertEqual(bank_payments[1].bank.bank_account_id, '60')
 
     def test_get_payments_empty(self):
-        fixture = get_path("card_payments_empty.csv")
-
-        payments = self.payment_processor.get_payments(fixture, "card")
-        self.assertEqual(len(payments), 0)
+        card_payments = self.payment_processor.get_payments(
+            self.empty_card_fixture, "card")
+        bank_payments = self.payment_processor.get_payments(
+            self.empty_bank_fixture, "bank")
+        self.assertEqual(len(bank_payments), 0)
+        self.assertEqual(len(card_payments), 0)
 
     def test_verify_payments(self):
         payment1 = create_stub_payment(mock_is_successful=True)
         payment2 = create_stub_payment(mock_is_successful=False)
         payment3 = create_stub_payment(mock_is_successful=True)
 
-        result = self.payment_processor.verify_payments([payment1, payment2, payment3])
+        result = self.payment_processor.verify_payments(
+            [payment1, payment2, payment3])
         self.assertEqual(result, [payment1, payment3])
