9ee8f7ccd18e3623eddcaa4823f6d4a83359fa16
Moses Mugisha <mossplix@gmail.com>
2019-09-06 11:45:21 +0300

    add failing test coverage for bank

--- tests/bricklane_platform/models/test_payment.py
+++ tests/bricklane_platform/models/test_payment.py
@@ -18,34 +18,51 @@ class TestPayment(unittest.TestCase):
 
     def test_init_with_data(self):
 
-        data = {
+        payment_data = [{
             "amount": "2000",
             "card_id": "45",
             "card_status": "processed",
             "customer_id": "123",
             "date": "2019-02-01",
-        }
+        }, {
+            "amount": "2000",
+            "bank_account_id": "45",
+            "customer_id": "123",
+            "date": "2019-02-01"
+        }]
+        for data in payment_data:
 
-        payment = Payment(data)
+            payment = Payment(data)
 
-        self.assertEqual(payment.customer_id, 123)
-        self.assertEqual(payment.date, datetime(2019, 2, 1))
-        self.assertEqual(payment.amount, 1960)
-        self.assertEqual(payment.fee, 40)
+            self.assertEqual(payment.customer_id, 123)
+            self.assertEqual(payment.date, datetime(2019, 2, 1))
+            self.assertEqual(payment.amount, 1960)
+            self.assertEqual(payment.fee, 40)
 
-        card = payment.card
-
-        self.assertIsInstance(card, Card)
-        self.assertEqual(card.card_id, 45)
-        self.assertEqual(card.status, "processed")
+            if payment.card:
+                card = payment.card
+                self.assertIsInstance(card, Card)
+                self.assertEqual(card.card_id, 45)
+                self.assertEqual(card.status, "processed")
+            if payment.bank:
+                bank = payment.bank
+                self.assertIsInstance(bank, Bank)
+                self.assertEqual(bank.bank_account_id, 45)
+                self.assertEqual(card.status, "processed")
 
     def test_is_successful(self):
         card = Card()
         card.status = "processed"
-        payment = Payment()
-        payment.card = card
+        payment1 = Payment()
+        payment1.card = card
 
-        self.assertTrue(payment.is_successful())
+        bank = Bank()
+        bank.status = "processed"
+        payment2 = Payment()
+        payment2.bank = bank
+
+        self.assertTrue(payment1.is_successful())
+        self.assertTrue(payment2.is_successful())
 
     def test_is_successful_declined(self):
         card = Card()
